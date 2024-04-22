d08f9cb65a65ca3e3c32312560dc89b352b50294
CecilC90 <c.cigan@gmx.net>
2024-04-22 20:02:57 +0200

    game logic

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
 
 
 @Component({
@@ -10,11 +11,37 @@ import { Component } from '@angular/core';
   styleUrl: './game.component.scss'
 })
 export class GameComponent {
-
   pickCardAnimation = false;
+  currentCard: string = '';
+  game!: Game;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
 
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      let nextCard = this.game.stack.pop();
+      if (nextCard != undefined) {
+        this.currentCard = nextCard;
+      }
+      
+      this.pickCardAnimation = true;
+      
+      console.log('New card: ' + this.currentCard);
+      console.log('Game is', this.game);
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
   }
 
 }
